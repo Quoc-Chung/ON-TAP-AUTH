0252071791bd17705f60be5773be2f31a8174656
Tu Quoc Chung <123059617+Quoc-Chung@users.noreply.github.com>
2025-06-22 10:31:14 +0700

    khoi tao

--- src/main/java/com/ontapauth/auth/dto/LoginRequest.java
+++ src/main/java/com/ontapauth/auth/dto/LoginRequest.java
@@ -1,5 +1,13 @@
 package com.ontapauth.auth.dto;
 
-public class LoginRequest {
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Data
+@Getter
+@Setter
+public class LoginRequest {
+     private String username;
+     private String password;
 }
